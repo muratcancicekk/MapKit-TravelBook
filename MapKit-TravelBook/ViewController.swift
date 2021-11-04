fbac2b06b50570f00c3eefe2e6143dc3b74a58e2
Murat Çiçek <62101026+muratcancicekk@users.noreply.github.com>
2021-11-04 12:47:15 +0300

    mapkit için gerekli fonksiyonlar yazılmaya başlandı

--- MapKit-TravelBook/ViewController.swift
+++ MapKit-TravelBook/ViewController.swift
@@ -7,15 +7,33 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
-class ViewController: UIViewController,MKMapViewDelegate {
-
+class ViewController: UIViewController,MKMapViewDelegate,CLLocationManagerDelegate {
+    
     @IBOutlet weak var mapView: MKMapView!
+    var locationManager = CLLocationManager()
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         mapView.delegate=self
+        
+        locationManager.delegate=self
+        locationManager.desiredAccuracy=kCLLocationAccuracyBest // kullanıcının yerini en iyi şekilde tespit etmemize yarar
+        locationManager.requestWhenInUseAuthorization()// kullanıcının yer takibini hangi durumlarda yapacağımızı seçtiğimiz yer
+        locationManager.startUpdatingLocation()
+        
+        
     }
-
-
+    
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) { // güncellenen lokasyonları bir dizide tutar
+        let location = CLLocationCoordinate2D(latitude: locations[0].coordinate.latitude, longitude: locations[0].coordinate.longitude)
+        let span = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+        let region = MKCoordinateRegion(center: location, span: span)
+        mapView.setRegion(region, animated: true)
+    }
+    
+    
 }
 
