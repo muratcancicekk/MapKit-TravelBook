4efb04465ac1db8c2a76defd095931efd4251c48
Murat Çiçek <62101026+muratcancicekk@users.noreply.github.com>
2021-11-04 12:19:59 +0300

    Initial Commit

---
+++ MapKit-TravelBook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MapKit-TravelBook
+//
+//  Created by Murat on 4.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MapKit-TravelBookUITests/MapKit_TravelBookUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MapKit_TravelBookUITestsLaunchTests.swift
+//  MapKit-TravelBookUITests
+//
+//  Created by Murat on 4.11.2021.
+//
+
+import XCTest
+
+class MapKit_TravelBookUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
